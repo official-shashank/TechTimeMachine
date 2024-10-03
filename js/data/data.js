2ac5945c3606f8e9c6636463a5ab242490adbb2e
Sohban2612 <sohbanahmad42@gmail.com>
2024-10-04 01:07:52 +0530

    iconic website jason ready

--- js/data/data.js
+++ js/data/data.js
@@ -230,98 +230,98 @@ var iconicWebsites=[
     {
       "title": "World Wide Web",
       "description": "Tim Berners-Lee launched the world’s first website on August 6th, 1991, originally to streamline information sharing between scientists.",
-      "imgUrl": "https://example.com/world-wide-web-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddb9ed6a207484123b7ce6_ozGFnp_ISpPaG6c1r8YFqwGkIh4PmFw7j5urAvjrn77noblCnIxkasfLbnY97-xb_KFVma-3GLxqSwMbeF37DUkV3QQuUq3measnlpRLICiPBjVY9irpljWH48fnZN9tpBcUdfsw.jpeg",
       "liveDemo": "http://info.cern.ch/",
       "articleUrl": "https://www.webdesignmuseum.org/website/world-wide-web"
     },
     {
       "title": "Space Jam",
       "description": "The official 1996 website promoting the Space Jam movie, full of 90s web design features like starry tiled backgrounds and interactive games.",
-      "imgUrl": "https://example.com/space-jam-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddb9ed55fdd4017838353b_zPX4UBBjtWeemWC2Ts6yDMxC26OmeytCnjFvCf57lHcwOVhingf-rZX5WwwU7NCBoq7ILa12-zB9ae5_gHMdy3C3t5iFAZvvcTe4aOzsnM4zfezw00efjoiTE_XwcKoBbyf6eClk.jpeg",
       "liveDemo": "http://www.spacejam.com/1996/",
       "articleUrl": "https://www.webdesignmuseum.org/website/space-jam"
     },
     {
       "title": "Yahoo Games",
       "description": "Launched in 1998, Yahoo Games hosted a wide range of free-to-play games with a simple design but addictive content.",
-      "imgUrl": "https://example.com/yahoo-games-image.jpg",
-      "liveDemo": "N/A",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddb9ed9a2589b582708f7f_rBJK9JD48hdaTaHIirMYQ_-YkqkuUKrX81F9sfROX2wnPF-UsyHgh2FuVljNuB1AzhiLoyo6NbOvdIx7fGlgM8Qf2_LcEBxWNqEEA7o7iGvWULy-Q9ED2ep-g7VjgJO64uCwQYwG.jpeg",
+      "liveDemo": "https://www.yahooinc.com/",
       "articleUrl": "https://www.webdesignmuseum.org/website/yahoo-games"
     },
     {
       "title": "San Francisco FogCam",
       "description": "The world’s oldest running webcam, created in 1994 to monitor fog conditions at San Francisco State University.",
-      "imgUrl": "https://example.com/fogcam-image.jpg",
-      "liveDemo": "http://www.fogcam.org/",
+      "imgUrl": "https://fogcam.org/fogcam2.jpg?Fri%20Oct%2004%202024%2000:22:51%20GMT+0530%20(India%20Standard%20Time)",
+      "liveDemo": "https://fogcam.org/",
       "articleUrl": "https://www.webdesignmuseum.org/website/fogcam"
     },
     {
       "title": "Hampster Dance",
       "description": "An iconic meme website from 1998, featuring rows of animated GIFs of hamsters and a never-ending catchy song.",
-      "imgUrl": "https://example.com/hampster-dance-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddb9ee26b35025a927617a_lyaskPVhs0RETPve4GxO59T1PFvlzaAewedvD2-Dg8eAMh7RdhLfJuXwkOK1fisJZb5Bl4MIPBpgdNHcq6-JuvxZ1pG9ZBeptrNVv6VAhh5RHlFWpNhMUTfj2HeiM7aUekGL6ITE.jpeg",
       "liveDemo": "http://www.hampsterdance.com/",
       "articleUrl": "https://www.webdesignmuseum.org/website/hampster-dance"
     },
     {
       "title": "AuctionWeb (eBay)",
       "description": "Launched in 1995 as a virtual auction platform, AuctionWeb — now known as eBay — revolutionized online shopping.",
-      "imgUrl": "https://example.com/auctionweb-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddb9ee75b3cbe0e81350d7_1HNsrkie7E2vy_-gvYuswkRtpJ0iaxolxD-gtXDJVpfYfK00miP9ftXtGNFyAyoEyn52RWrXNP2tAItmWTy0dSIbCFysO4m4A0LeuKRUffenNyhMvRPsmCug5mhkmLh1mOxZ0KlS.jpeg",
       "liveDemo": "https://www.ebay.com/",
       "articleUrl": "https://www.webdesignmuseum.org/website/auctionweb"
     },
     {
       "title": "CNN O.J. Simpson Trial",
       "description": "CNN’s dedicated section for O.J. Simpson’s 1995 murder trial, complete with trial transcripts, evidence, and player profiles.",
-      "imgUrl": "https://example.com/cnn-oj-trial-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddbb1084bdd14f9295eb8a_Fz-CwHkz8aMkZOVA6mebbSpxFu-iNkgCk8qILXRW7F7yGpaznFY66dCx0RIH-Xx1fXU2TNogGHl-PI9JAvTxzgncGqlz95iZyHitqDfB-ZHANnTHQolgnR9UYxMeQ0gcuHFRvWsV.png",
       "liveDemo": "https://edition.cnn.com/SPECIALS/oj/",
       "articleUrl": "https://www.webdesignmuseum.org/website/cnn-oj-simpson-trial"
     },
     {
       "title": "Pitchfork",
       "description": "Online music magazine Pitchfork launched in 1995, becoming a go-to source for indie music reviews and news.",
-      "imgUrl": "https://example.com/pitchfork-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddbb106a207421773c861b_Lp0-glcQSTncmRcpgJSUHQ9lm0RE7wM6o6wVsXzh5LgZ_vPibLb6-7oAaYck0exkpU7tEEc-BsiTThun0v7HdZSICpO45l0mTZ3iGvZWn42pb5oX5C39n-ZSiva8EESxvXQN51v1.png",
       "liveDemo": "https://pitchfork.com/",
       "articleUrl": "https://www.webdesignmuseum.org/website/pitchfork"
     },
     {
       "title": "PizzaNet",
       "description": "Pizza Hut’s 1994 experiment in online food ordering, where visitors could order pizza through a form and pay in cash on delivery.",
-      "imgUrl": "https://example.com/pizzanet-image.jpg",
-      "liveDemo": "https://example.com/pizzanet-archive",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddbb0fb9c6233253412921_cABQru5pPDRqtUVqFrU-3H0W3iXDpD1Pyf3OvbZ-KNBSNbn8i5L7xT0PUTxjwILjqCMpZt7gv0v-n3172ukXfq88obWWiH7XlPoqvHVY7Zx1PBJ1Ar6-PQNlu1deJBa79VNCVdhB.png",
+      "liveDemo": "https://thehistoryoftheweb.com/postscript/pizzanet/",
       "articleUrl": "https://www.webdesignmuseum.org/website/pizzanet"
     },
     {
       "title": "NASA",
       "description": "NASA’s first website launched in 1994, offering centralized access to space-related news, images, and updates on space missions.",
-      "imgUrl": "https://example.com/nasa-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddbb1084bdd197ed95ebd1_RbVljegBYEj629kTeNTVLDPeKWsG4qukQAWVQu-IIxJRJh85378NdvAXXg7scLOPpbgdo0QfX2nfV7qR6y8YNHudDcGzpR1lCdOcO9bwg-DIjFmsD_8xi0rYo7lvb26nS0hPKNuP.jpeg",
       "liveDemo": "https://www.nasa.gov/",
       "articleUrl": "https://www.webdesignmuseum.org/website/nasa"
     },
     {
       "title": "MapQuest",
       "description": "Launched in 1996, MapQuest helped drivers map out routes long before car GPS devices, making it an early favorite for road trips.",
-      "imgUrl": "https://example.com/mapquest-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddbb10b10b2958cfb75afa_k6keaSHL8_BcvdOGQBcYvpJjo67edGz4fbGwoLvP3X7e4wj-n1lXQffjq_oKH7ijOLXjEvmIgTl4DSwWbV6ZVoyMXs1wPBo8Gg-PkyHXDvNwpir8kQASNfAiOOtadwzwSruAtGWm.png",
       "liveDemo": "https://www.mapquest.com/",
       "articleUrl": "https://www.webdesignmuseum.org/website/mapquest"
     },
     {
       "title": "LEGO",
       "description": "LEGO’s 1996 website catered to kids with interactive games and low-resolution animated GIFs of spinning LEGO bricks.",
-      "imgUrl": "https://example.com/lego-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddbb1075b3cb139513b682_F_GCv0Sut7OuwKvdS9ZthTPMwDakJn4DTrShAslDZ8Kj7TaEypbyXl5pXXET6P303AsQ76XQqFIhiRrdJM-K_zxaa6eZ0JkJ5WQ3XoPOSTrTxgH2LLm-GsvszGc4n5huEGlS-eLY.png",
       "liveDemo": "https://www.lego.com/",
       "articleUrl": "https://www.webdesignmuseum.org/website/lego"
     },
     {
       "title": "Webaholics",
       "description": "Launched in 1994, Webaholics was a directory of top links and a support forum for people who felt addicted to the internet.",
-      "imgUrl": "https://example.com/webaholics-image.jpg",
-      "liveDemo": "N/A",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddbb111a199979c9ff2d2b_3IPVU_Wbfj8IXkWv0c44wuwbyq-1wGlZH9mVEMr0ay0LZJSGJVeMXHd1jNxbhMKY4_uxRZCzk40g6we2a5cLcT5p5k_La6uIE6GOPukJKhv8uoKmFpcRnI4It1CVmbGwB7GzhiuZ.png",
+      "liveDemo": "https://www.chicagotribune.com/1996/06/26/oh-no-another-addiction-how-the-mental-health-community-views-the-problem-a-place-for-webaholics/",
       "articleUrl": "https://www.webdesignmuseum.org/website/webaholics"
     },
     {
       "title": "Goosebumps",
       "description": "The official Goosebumps website launched in 1996, featuring monster-themed navigation and spooky e-greetings.",
-      "imgUrl": "https://example.com/goosebumps-image.jpg",
+      "imgUrl": "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/61ddbb10c0763c5ece7e79b8_fi88IJlDR0LLY6b1XUbl_scERn2op6X-AK9w_lOuN3rASHdDZciJGm8BJLwC33KbqaIVPXNrq726sfYcxN5CBANSy3EGoIeysguQQhxbziOGOF2JpmwnyVTb8BsOsISXt2sbW4oY.png",
       "liveDemo": "https://www.scholastic.com/goosebumps/",
       "articleUrl": "https://www.webdesignmuseum.org/website/goosebumps"
     }
